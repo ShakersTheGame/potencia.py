df17ca473b861abcab077c4e2aa816e15ea16e95
felisbertosewy <96011281+ShakersTheGame@users.noreply.github.com>
2024-02-26 00:02:14 +0000

    Add files via upload

---
+++ mediana.c
@@ -0,0 +1,25 @@
+#   include <stdio.h>
+
+int mediana(int a, int b, int c) {
+    int med;
+
+    if (a > b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    if (a > c) {
+        int temp = a;
+        a = c;
+        c = temp;
+    }
+    if (b > c) {
+        int temp = b;
+        b = c;
+        c = temp;
+    }
+
+    med = b;
+
+    return med;
+}
